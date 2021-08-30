06e4d6d0974bcf287ee42d89647371a2a48e141e
Dmitry Chernyshev <chernyshevdv@gmail.com>
2021-08-30 22:23:47 +0300

    GuessTheFlag project added

---
+++ apps/2_GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Dmitry Chernyshev on 29.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
